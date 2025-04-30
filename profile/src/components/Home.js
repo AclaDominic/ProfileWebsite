07ba7618cc657c7e6308bb2b44d78e08001954f1
default <default@default.default>
2025-04-30 12:19:24 +0800

    Color change

--- profile/src/components/Home.js
+++ profile/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
  
 const Home = () => {
     return (
-        <div className="home-page" style={{ backgroundColor: '#e6f2e6', color: '#006600', padding: '20px' }}>
+        <div className="home-page" style={{ backgroundColor: '#e6f2e6', color: 'black', padding: '20px' }}>
             <h1>Welcome to My Profile!</h1>
             <p>Hello! I'm Acla, Dominic A.</p>
             <p>I am passionate about coding and aim to build modern web applications and solving or giving solutions to making logics.</p>
