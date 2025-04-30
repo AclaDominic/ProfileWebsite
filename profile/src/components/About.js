cc2e8e1090e8f903c7954f8942f56038e38b779f
default <default@default.default>
2025-04-30 11:58:30 +0800

    Contact

--- profile/src/components/About.js
+++ profile/src/components/About.js
@@ -7,13 +7,7 @@ const About = () => {
       <h1>About Me</h1>
       <p>Dangal Greetings! I'm Acla, Dominic A.</p>
 
-      <p>Feel free to reach out!</p>
-      <p>
-        Email: <a href="mailto:acladominic10@gmail.com">acladominic10@gmail.com</a>
-      </p>
-      <p>
-        GitHub: <a href="https://github.com/AclaDominic" target="_blank" rel="noopener noreferrer">AclaDominic</a>
-      </p>
+      
 
     </div>
   )
