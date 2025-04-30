98ea4d7602ebf07265e0dd34039a64b27d09bc75
default <default@default.default>
2025-04-30 12:13:52 +0800

    Spelling

--- profile/src/components/About.js
+++ profile/src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <h1>About Me</h1>
       <p>Dangal Greetings! I'm Acla, Dominic A.</p>
 
-      <p>I like computers and mmy hobby is to play games. My interests are thins that keep my head ocuppied</p>
+      <p>I like computers and mmy hobby is to play games. My interests are things that keep my head ocuppied</p>
 
     </div>
   )
