028d8724861b584038738a8c0fcba0764d2842dc
default <default@default.default>
2025-04-30 12:03:49 +0800

    added name on home

--- profile/src/components/Home.js
+++ profile/src/components/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div className="home-page" style={{ backgroundColor: '#e6f2e6', color: '#006600', padding: '20px' }}>
             <h1>Welcome to My Profile!</h1>
+            <p>Hello! I'm Acla, Dominic A.</p>
             <p>I am passionate about coding and aim to build modern web applications that make a difference.</p>
             <p>I already learned a bit of HTML, CSS, and JavaScript.As well as some of Java, C#, and C++</p>
         </div>
