77d7b22b93d237bb3b0eaf9daa7ebc40568fb289
default <default@default.default>
2025-04-30 12:03:12 +0800

    update

--- profile/src/components/Home.js
+++ profile/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <div className="home-page" style={{ backgroundColor: '#e6f2e6', color: '#006600', padding: '20px' }}>
             <h1>Welcome to My Profile!</h1>
             <p>I am passionate about coding and aim to build modern web applications that make a difference.</p>
-            <p>I already learned a bit of HTML, CSS, and JavaScript.As well as Java, C#, and C++</p>
+            <p>I already learned a bit of HTML, CSS, and JavaScript.As well as some of Java, C#, and C++</p>
         </div>
     );
 };
